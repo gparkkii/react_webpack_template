97e0b61e05b7bc0070a562ec7feea2388a311438
gparkkii <jyp933@gmail.com>
2021-04-10 17:32:11 +0900

    chore :arrow_right: dist 폴더 자동 삭제 플러그인 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   name: 'webpack-config',
@@ -28,6 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new CleanWebpackPlugin(['dist'])
   ]
 }
